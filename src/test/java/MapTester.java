61dfbd321adfcdd0dd9ae936a5466ca0d5729fce
Majsan3k <maja.lund@live.se>
2017-10-09 13:40:19 +0200

    add test to place player

--- src/test/java/MapTester.java
+++ src/test/java/MapTester.java
@@ -4,10 +4,18 @@ import static junit.framework.TestCase.assertEquals;
 
 public class MapTester {
 
+    Map map = new Map(10, 20);
+
     @Test
     public void testCreateMap(){
-        Map map = new Map(10, 20);
         assertEquals(10, map.getWidth());
         assertEquals(20, map.getHeight());
+    }
+
+    @Test
+    public void testPlacePlayer(){
+        Player player = new Player("Maja");
+        map.placePlayer(player);
+
     }
 }

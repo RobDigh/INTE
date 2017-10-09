8ef7b12b467706d142e752f6293f66f3367c1ff5
Simon Olofsson <s.olofsson@gmail.com>
2017-10-09 10:30:32 +0200

    Wrote test for negative hp to Player constructor.

--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -6,13 +6,22 @@ import static org.junit.Assert.assertEquals;
 
 public class PlayerTest {
 
+    private Player createPlayerWithCustomHPAndSpeed(int hp, int speed) {
+        return new Player(hp, speed);
+    }
+
     @Test
     public void testConstructPlayerWithValidHPAndSpeed() {
 
-        Player player = new Player(100, 1);
+        Player player = createPlayerWithCustomHPAndSpeed(100, 1);
 
         assertEquals(100, player.getHP());
         assertEquals(1, player.getSpeed());
 
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructPlayerWithNegativeHP() {
+        createPlayerWithCustomHPAndSpeed(-1, 1);
     }
 }

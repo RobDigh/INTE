266e64d4f64d0c5a8a2b1b60ff98d5873bd86996
Sigoria <appelgrenjohanna@gmail.com>
2017-10-09 13:56:57 +0200

    Added constructor for entity and modified getLives() to return variable value instead of hard coded

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -2,7 +2,11 @@
 public class Entity {
     private int lives;
 
-    public int getLives(){
-        return 10;
+    public Entity() {
+        this.lives = 10;
+    }
+
+    public int getLives() {
+        return lives;
     }
 }

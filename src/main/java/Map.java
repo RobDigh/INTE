c54301a980c7cf60b3d3e8abf059288459375f9b
Majsan3k <maja.lund@live.se>
2017-10-09 13:32:59 +0200

    test create map. get width/height fails

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,2 +1,14 @@
 public class Map {
+
+    public Map(int width, int height){
+
+    }
+
+    public int getWidth(){
+        return 0;
+    }
+
+    public int getHeight(){
+        return 0;
+    }
 }
--- src/test/java/MapTester.java
+++ src/test/java/MapTester.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class MapTester {
 
     @Test
     public void testCreateMap(){
-        
+        Map map = new Map(10, 20);
+        assertEquals(10, map.getWidth());
+        assertEquals(10, map.getHeight());
     }
 }

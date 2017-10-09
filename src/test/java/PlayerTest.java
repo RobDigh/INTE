0c0937d592c8bfdcaaed965b3aa9644177bd3d84
Majsan3k <maja.lund@live.se>
2017-10-09 12:05:03 +0200

    started work

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void createPlayerWithDefaultValues(){
+
+        Player player = new Player("Maja");
+        
+    }
+
+}

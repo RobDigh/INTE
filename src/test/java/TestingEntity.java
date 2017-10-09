20ea4c46364d7f764ee1ae9d9fd61efdba158d00
Sigoria <appelgrenjohanna@gmail.com>
2017-10-09 13:57:55 +0200

    Modifying test for lives to accept arguments in Entity-constructor

--- src/test/java/TestingEntity.java
+++ src/test/java/TestingEntity.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class TestingEntity {
     @Test
     public void testLives(){
-        Entity entity = new Entity();
+        Entity entity = new Entity(10);
         assertEquals(10, entity.getLives());
     }
 

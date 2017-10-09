0903c54427bd61e805bf5d496b7d57c348293ab5
rodi0231 <rodi0231@dsv.su.se>
2017-10-09 12:15:39 +0200

    Passed testCreateOnePlayer

--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -2,15 +2,20 @@ package player;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
 
     @Test
-    public void testCreatePlayer(){
+    public void testCreateOnePlayer(){
 
+        List<Player> playerList = new ArrayList();
         Player player = new Player();
-
+        playerList.add(player);
+        assertEquals(player, playerList.get(0));
     }
 
 }

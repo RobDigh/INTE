a3065169d23a7b9ea3bc1d3a3febdd674e2e2633
rodi0231 <rodi0231@dsv.su.se>
2017-10-09 12:02:33 +0200

    Created testPlayer and Player class

---
+++ src/main/java/player/Player.java
@@ -0,0 +1,10 @@
+package player;
+
+public class Player {
+
+    public Player(){
+
+    }
+
+
+}
---
+++ src/test/java/player/PlayerTest.java
@@ -0,0 +1,16 @@
+package player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void testCreatePlayer(){
+
+        Player player = new Player();
+
+    }
+
+}

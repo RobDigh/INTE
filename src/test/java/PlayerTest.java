68ec507b08dacd0fdbd4a8796f63ece4fd7bc223
Majsan3k <maja.lund@live.se>
2017-10-09 12:33:30 +0200

    addThreeLives test

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,20 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class PlayerTest {
 
+    Player player = new Player("Maja");
+
     @Test
     public void createPlayerWithDefaultValues(){
-        Player player = new Player("Maja");
         assertEquals("Maja", player.getName());
         assertEquals(5, player.getLives());
     }
 
     @Test
     public void increaseLives(){
-        Player player = new Player("Maja");
         player.addLife();
         assertEquals(6, player.getLives());
+    }
+
+    @Test
+    public void addThreeLives(){
+        int lives = player.getLives();
+        for(int i = 1; i < 5; i++){
+            player.addLife();
+            assertEquals(lives + i, player.getLives());
+        }
     }
 }

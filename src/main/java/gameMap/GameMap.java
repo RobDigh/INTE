03be0268bbd417a495589448f6acfb2ce0214611
Simon Olofsson <s.olofsson@gmail.com>
2017-10-09 10:54:31 +0200

    Refactored GameMap to utilize instance variables for getting x and y lengths. Tests passing.

--- src/main/java/gameMap/GameMap.java
+++ src/main/java/gameMap/GameMap.java
@@ -5,15 +5,28 @@ public class GameMap {
     public static final int DEFAULT_X_LENGTH = 10;
     public static final int DEFAULT_Y_LENGTH = 10;
 
+    private int xLength;
+    private int yLength;
+
     public GameMap() {
+
+        this.xLength = DEFAULT_X_LENGTH;
+        this.yLength = DEFAULT_Y_LENGTH;
+
+    }
+
+    public GameMap(int xLength, int yLength) {
+
+        this.xLength = xLength;
+        this.yLength = yLength;
 
     }
 
     public int getXLength() {
-        return DEFAULT_X_LENGTH;
+        return xLength;
     }
 
     public int getYLength() {
-        return DEFAULT_Y_LENGTH;
+        return yLength;
     }
 }

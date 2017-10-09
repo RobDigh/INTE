10bd60c491a71cd1e76750f0a5968550cb87c114
Sigoria <appelgrenjohanna@gmail.com>
2017-10-09 15:53:53 +0200

    Added controller for negative speed

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -6,9 +6,12 @@ public class Entity {
     public Entity(int initialValueLives, int initialSpeed) {
         this.lives = initialValueLives;
         if (initialValueLives < 1) {
-            throw new IllegalArgumentException("starting lives can't be less than 1");
+            throw new IllegalArgumentException("Starting lives can't be less than 1");
         }
         this.speed = initialSpeed;
+        if(initialSpeed<0){
+            throw new IllegalArgumentException("Speed can't be less than 0");
+        }
     }
 
     public int getLives() {

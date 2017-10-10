9c9fee2daa1ee9be913ed8d994ae2316d5bb1f30
Sigoria <appelgrenjohanna@gmail.com>
2017-10-10 18:26:31 +0200

    wrote test for placing player on map

--- src/test/java/MapTests.java
+++ src/test/java/MapTests.java
@@ -1,8 +1,9 @@
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
 import static org.junit.Assert.*;
+
+import java.awt.*;
 
 public class MapTests {
     @Rule
@@ -13,6 +14,15 @@ public class MapTests {
         Map map = new Map(30, 20);
         assertEquals(30, map.getWidth());
         assertEquals(20, map.getHeight());
+    }
+
+    @Test
+    public void testPlayerPlacement(){
+        Map map = new Map(29, 19);
+        PlayerCharacter pc = new PlayerCharacter();
+        Point playerStartingPoint = new Point(15, 10);
+        map.placePlayer(pc, playerStartingPoint);
+        assertEquals(playerStartingPoint, map.getPlayerPosition);
     }
 
 }

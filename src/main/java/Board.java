3ea339bf4d0c8ff4f92f2ff8edf025f02b2c3c14
Chumu-J <chumu_jamal@gmail.com>
2017-10-10 23:36:06 +0200

    moved map init to creat map method

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -6,25 +6,31 @@ public class Board {
 	private int boardXvalue;
 	private int boardYvalue;
 	
+	public int [][] map;
+	
 	public void setDefaultBoardValues(){
 		this.boardXvalue = DEFAULT_BOARD_LENGTH;
 		this.boardYvalue = DEFAULT_BOARD_HEIGHT;
 	}
 	
-	public void createCustomBoard(int newX, int newY){
+	public void createCustomBoardSize(int newX, int newY){
 		
 			if ( newX < 0 || newY < 0 ){
 				throw new IllegalArgumentException("Board X/Y values can't be negative!");
 			} else if (newX < 10 || newY < 10){
 				throw new IllegalArgumentException("Board X/Y values can't be less than 10");
 			}else{
-				
 				boardXvalue = newX;
 				boardYvalue = newY;
 			}
 
 		}
+	
+	public void createMap(){
+		map = new int[this.boardXvalue][this.boardYvalue];
+	}
 
+	
 	
 	public int getBoardValueX(){
 		return boardXvalue;

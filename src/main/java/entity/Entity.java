f1f8cd229c32c181ca3943812e3c58eb6478ae2d
Simon Olofsson <s.olofsson@gmail.com>
2017-10-10 17:37:40 +0200

    Refactored GameMap to use single data structure for all entities.

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -4,6 +4,10 @@ import gameMap.GameMap;
 
 public abstract class Entity {
 
+    public int getSpeed() {
+        return 0;
+    }
+
     public abstract boolean visit(Entity entity, GameMap environment);
 
 }

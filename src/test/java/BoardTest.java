4a8ba75299b509bcdecd86e474f5e555fb411d16
Chumu-J <chumu_jamal@gmail.com>
2017-10-10 15:04:33 +0200

    added new test for creating custom sized board, test will fail

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -17,4 +17,14 @@ public class BoardTest {
 		assertEquals(10, sut.getBoardValueY());
 	}
 	
+	@Test
+	public void createCustomBoardSizeTest(){
+		//given
+		Board sut = new Board();
+		//when
+		sut.createCustomBoard();
+		//then
+		assertEquals(20, sut.getBoardValueX());
+		assertEquals(20, sut.getBoardValueY());
+	}
 }

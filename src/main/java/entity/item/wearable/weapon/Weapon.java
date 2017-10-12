b165a2ace2ea9e29e37874fa5c5146f3bc1fcedb
Simon Olofsson <s.olofsson@gmail.com>
2017-10-12 09:02:05 +0200

    Wrote minimal code to make tests compile and run.

--- src/main/java/entity/item/wearable/weapon/Weapon.java
+++ src/main/java/entity/item/wearable/weapon/Weapon.java
@@ -9,12 +9,12 @@ public class Weapon extends Wearable {
         super(damageBonus);
     }
 
-    public int getDamageBonus() {
-        return getBonus();
+    public int getBonus() {
+        return super.getBonus();
     }
 
     @Override
     public void affect(Creature creature) {
-        creature.incrementDamageBonus(getDamageBonus());
+        creature.incrementDamageBonus(getBonus());
     }
 }

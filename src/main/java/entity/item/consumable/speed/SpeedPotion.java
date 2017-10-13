bbf4cb80631e6fbfb2715505e16271b372f91c20
Simon Olofsson <s.olofsson@gmail.com>
2017-10-13 06:39:31 +0200

    Wrote minimal code to pass tests.

--- src/main/java/entity/item/consumable/speed/SpeedPotion.java
+++ src/main/java/entity/item/consumable/speed/SpeedPotion.java
@@ -4,15 +4,17 @@ import entity.Creature;
 
 public class SpeedPotion {
 
-    public SpeedPotion(int bonus) {
+    private int bonus;
 
+    public SpeedPotion(int bonus) {
+        this.bonus = bonus;
     }
 
     public int getBonus() {
-        return -1;
+        return bonus;
     }
 
     public void affect(Creature creature) {
-        creature.gainSpeed(-1);
+        creature.gainSpeed(getBonus());
     }
 }

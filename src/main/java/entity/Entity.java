0053f2bebac0ad01397ec32203c2a96c3ee03807
Simon Olofsson <s.olofsson@gmail.com>
2017-10-13 07:50:24 +0200

    Added javadoc for Entity.accept().

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -8,6 +8,19 @@ public abstract class Entity {
         return 0;
     }
 
+    /**
+     * Resolves an interaction between the entity the method is invoked on and
+     * the entity passed.
+     *
+     * @param entity The entity that visits the entity on which the method is
+     *               invoked.
+     * @param environment The environment the interaction takes place in.
+     *                    Primarily used to remove the entity on which the
+     *                    method is called if it, for example, loses combat.
+     * @return true if the visiting entity should be readded to the,
+     * environment, false if the visiting entity should be removed from the
+     * environment.
+     */
     public abstract boolean accept(Entity entity, GameMap environment);
 
 }

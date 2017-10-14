9f140cb161726700c7b0eb5531349273c61baf6f
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 22:08:53 +0200

    Added getConstitution and values in Breed, tests compile and pass

--- src/main/java/entity/creature/Breed.java
+++ src/main/java/entity/creature/Breed.java
@@ -1,25 +1,27 @@
 package entity.creature;
 
 public enum Breed {
-    BULLDOG("Bulldog", 6, 5),
-    CHIHUAHUA("Chihuahua", 5, 8),
-    DACHSHUND("Dachshund", 5, 7),
-    DOBERMANN("Dobermann", 7, 6),
-    GREYHOUND("Greyhound", 5, 6),
-    LABRADOR("Labrador", 6, 6),
-    OVTJARKA("Ovtjarka", 8, 5),
-    SIBERIANHUSKY("Siberian husky", 6, 7),
-    STBERNARD("St. Bernard", 7, 5),
-    VIZSLA("Vizsla", 5, 5);
+    BULLDOG("Bulldog", 6, 5, 7),
+    CHIHUAHUA("Chihuahua", 5, 8, 5),
+    DACHSHUND("Dachshund", 5, 7, 6),
+    DOBERMANN("Dobermann", 7, 6, 5),
+    GREYHOUND("Greyhound", 5, 6, 7),
+    LABRADOR("Labrador", 6, 6, 6),
+    OVTJARKA("Ovtjarka", 8, 5, 5),
+    SIBERIANHUSKY("Siberian husky", 6, 7, 5),
+    STBERNARD("St. Bernard", 7, 5, 6),
+    VIZSLA("Vizsla", 5, 5, 8);
 
     private String name;
     private int strength;
     private int dexterity;
+    private int constitution;
 
-    Breed(String name, int strength, int dexterity) {
+    Breed(String name, int strength, int dexterity, int constitution) {
         this.name = name;
         this.strength = strength;
         this.dexterity = dexterity;
+        this.constitution = constitution;
     }
 
     public String getName() {
@@ -32,5 +34,9 @@ public enum Breed {
 
     public int getDexterity() {
         return dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
     }
 }

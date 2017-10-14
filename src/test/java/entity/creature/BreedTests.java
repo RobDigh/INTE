6cc5ea02200eb40edb940ebb0db329dbdb7120da
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 21:37:34 +0200

    Added test to assert LABRADOR exists in Breed enum. Code does not compile

--- src/test/java/entity/creature/BreedTests.java
+++ src/test/java/entity/creature/BreedTests.java
@@ -1,4 +1,13 @@
 package entity.creature;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BreedTests {
+
+    @Test
+    public void testAllValuesAreThere() {
+        assertNotNull(Breed.valueOf("LABRADOR"));
+    }
 }

fffb5dd65c8b71b6c5ea5091386628fe881ef299
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 20:46:45 +0200

    Added test for new constructor. Code does not compile

--- src/test/java/entity/creature/CreatureConstructorTest.java
+++ src/test/java/entity/creature/CreatureConstructorTest.java
@@ -1,4 +1,11 @@
 package entity.creature;
 
 public class CreatureConstructorTest {
+
+    public void testCreateCreature(){
+        Creature creature = new Creature(5, 5, 5);
+        assertEquals(5, creature.getStrength());
+        assertEquals(5, creature.getDexterity());
+        assertEquals(5, creature.getConstitution());
+    }
 }

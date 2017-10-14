1b9e8cb27832e1ae59163091aeecb1f3cfb060e9
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 22:36:36 +0200

    changed getBreed to return value of new breed variable, and wroe method to calculate breed based on stats. Tests pass

--- src/main/java/entity/creature/PlayerCharacter.java
+++ src/main/java/entity/creature/PlayerCharacter.java
@@ -1,12 +1,32 @@
 package entity.creature;
 
-public class PlayerCharacter extends Creature{
+import java.util.ArrayList;
+import java.util.EnumSet;
 
-    public PlayerCharacter(int strength, int dexterity, int constitution){
+public class PlayerCharacter extends Creature {
+    private Breed breed;
+
+    public PlayerCharacter(int strength, int dexterity, int constitution) {
         super(strength, dexterity, constitution);
+
+        breed = calculateBreed(strength, dexterity, constitution);
+        if (breed == null) {
+            throw new IllegalArgumentException("There is no breed that corresponds with these stat values");
+        }
     }
 
-    public Breed getBreed(){
-        return Breed.CHIHUAHUA;
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public Breed calculateBreed(int strength, int dexterity, int constitution) {
+        ArrayList<Breed> temp = new ArrayList<>(EnumSet.allOf(Breed.class));
+        for (int i = 0; i < temp.size(); i++) {
+            Breed breed = temp.get(i);
+            if (breed.getStrength() == strength && breed.getDexterity() == dexterity && breed.getConstitution() == constitution) {
+                return breed;
+            }
+        }
+        return null;
     }
 }

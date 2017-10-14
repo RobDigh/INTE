37f12b4dea50e17df06a11f59fa027ada8a4d02e
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 22:13:50 +0200

    Added tests for Dobermann and Greyhound, all tests pass

--- src/test/java/entity/creature/BreedTests.java
+++ src/test/java/entity/creature/BreedTests.java
@@ -20,6 +20,7 @@ public class BreedTests {
         assertNotNull(Breed.valueOf("VIZSLA"));
     }
 
+    @Test
     public void testAllBulldogValues() {
         assertEquals("Bulldog", Breed.BULLDOG.getName());
         assertEquals(6, Breed.BULLDOG.getStrength());
@@ -27,6 +28,7 @@ public class BreedTests {
         assertEquals(7, Breed.BULLDOG.getConstitution());
     }
 
+    @Test
     public void testAllChihuahuaValues() {
         assertEquals("Chihuahua", Breed.CHIHUAHUA.getName());
         assertEquals(5, Breed.CHIHUAHUA.getStrength());
@@ -34,10 +36,27 @@ public class BreedTests {
         assertEquals(5, Breed.CHIHUAHUA.getConstitution());
     }
 
+    @Test
     public void testAllDachshundValues(){
         assertEquals("Dachshund", Breed.DACHSHUND.getName());
         assertEquals(5, Breed.DACHSHUND.getStrength());
         assertEquals(7, Breed.DACHSHUND.getDexterity());
         assertEquals(6, Breed.DACHSHUND.getConstitution());
+    }
+
+    @Test
+    public void testAllDobermannValues(){
+        assertEquals("Dobermann", Breed.DOBERMANN.getName());
+        assertEquals(7, Breed.DOBERMANN.getStrength());
+        assertEquals(6, Breed.DOBERMANN.getDexterity());
+        assertEquals(5, Breed.DOBERMANN.getConstitution());
+    }
+
+    @Test
+    public void testAllGreyhoundValues(){
+        assertEquals("Greyhound", Breed.GREYHOUND.getName());
+        assertEquals(5, Breed.GREYHOUND.getStrength());
+        assertEquals(6, Breed.GREYHOUND.getDexterity());
+        assertEquals(7, Breed.GREYHOUND.getConstitution());
     }
 }

13d1d67e87ae5b5b0382ee25455d35acff1dadbe
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 21:14:33 +0200

    Added new PlayerCharacter subclass and standard constructor to make tests pass. All tests compile and pass

---
+++ src/main/java/entity/creature/PlayerCharacter.java
@@ -0,0 +1,8 @@
+package entity.creature;
+
+public class PlayerCharacter extends Creature{
+
+    public PlayerCharacter(int strength, int dexterity, int constitution){
+        super(strength, dexterity, constitution);
+    }
+}

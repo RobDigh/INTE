455961678c59d57ca0ad16dbefaf530cbc7a5056
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 22:05:41 +0200

    Added getDexterity and values in Breed, code compiles and all tests pass

--- src/main/java/entity/creature/Breed.java
+++ src/main/java/entity/creature/Breed.java
@@ -1,30 +1,36 @@
 package entity.creature;
 
 public enum Breed {
-    BULLDOG("Bulldog", 6),
-    CHIHUAHUA("Chihuahua", 5),
-    DACHSHUND("Dachshund", 5),
-    DOBERMANN("Dobermann", 7),
-    GREYHOUND("Greyhound", 5),
-    LABRADOR("Labrador", 6),
-    OVTJARKA("Ovtjarka", 8),
-    SIBERIANHUSKY("Siberian husky", 6),
-    STBERNARD("St. Bernard", 7),
-    VIZSLA("Vizsla", 5);
+    BULLDOG("Bulldog", 6, 5),
+    CHIHUAHUA("Chihuahua", 5, 8),
+    DACHSHUND("Dachshund", 5, 7),
+    DOBERMANN("Dobermann", 7, 6),
+    GREYHOUND("Greyhound", 5, 6),
+    LABRADOR("Labrador", 6, 6),
+    OVTJARKA("Ovtjarka", 8, 5),
+    SIBERIANHUSKY("Siberian husky", 6, 7),
+    STBERNARD("St. Bernard", 7, 5),
+    VIZSLA("Vizsla", 5, 5);
 
     private String name;
     private int strength;
+    private int dexterity;
 
-    Breed(String name, int strength) {
+    Breed(String name, int strength, int dexterity) {
         this.name = name;
         this.strength = strength;
+        this.dexterity = dexterity;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getStrength(){
+    public int getStrength() {
         return strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
     }
 }

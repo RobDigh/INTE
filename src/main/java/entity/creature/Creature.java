3bf79d90a1a796c3a47373100f10f73940b818cf
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 20:29:57 +0200

    Wrote get-methods for Str, Dex and Con. Minimal code to make tests compile and pass

--- src/main/java/entity/creature/Creature.java
+++ src/main/java/entity/creature/Creature.java
@@ -47,12 +47,24 @@ public class Creature extends Entity {
         return speed;
     }
 
-    public double getDamageReduction(){
+    public double getDamageReduction() {
         return damageReduction;
     }
 
-    public double getDamageBonus(){
+    public double getDamageBonus() {
         return damageBonus;
+    }
+
+    public int getStrength() {
+        return 5;
+    }
+
+    public int getDexterity() {
+        return 6;
+    }
+
+    public int getConstitution() {
+        return 7;
     }
 
     public void gainHP(int amount) {
@@ -97,68 +109,68 @@ public class Creature extends Entity {
     }
 
     public void incrementDamageReduction(double increaseValue) {
-        if(increaseValue <= 0){
+        if (increaseValue <= 0) {
             throw new IllegalArgumentException("Increase value must be greater than 0");
         }
-        if(increaseValue > 100){
+        if (increaseValue > 100) {
             throw new IllegalArgumentException("Increase value must be less or equal to 100");
         }
-        if((damageReduction + increaseValue) >= 100){
+        if ((damageReduction + increaseValue) >= 100) {
             damageReduction = 100;
-        }else{
+        } else {
             double newDamageReduction = Math.round(increaseValue * 100);
             damageReduction += (newDamageReduction / 100);
         }
     }
 
-    public void decrementDamageReduction(double decreaseValue){
-        if(decreaseValue <= 0){
+    public void decrementDamageReduction(double decreaseValue) {
+        if (decreaseValue <= 0) {
             throw new IllegalArgumentException("Decrease value must be greater than 0");
         }
-        if(decreaseValue > 100){
+        if (decreaseValue > 100) {
             throw new IllegalArgumentException("Decrease value must be less or equal to 100");
         }
-        if((damageReduction - decreaseValue) <= 0){
+        if ((damageReduction - decreaseValue) <= 0) {
             damageReduction = 0;
-        }else {
+        } else {
             double newDamageReduction = Math.round((damageReduction - decreaseValue) * 100);
             damageReduction = newDamageReduction / 100;
         }
     }
 
     public void incrementDamageBonus(double increaseValue) {
-        if(increaseValue <= 0){
+        if (increaseValue <= 0) {
             throw new IllegalArgumentException("Damage bonus must be greater than 0");
         }
-        if(increaseValue > 100){
+        if (increaseValue > 100) {
             throw new IllegalArgumentException("Damage bonus must be lower or equal to 100");
         }
-        if((damageBonus + increaseValue) > 100){
+        if ((damageBonus + increaseValue) > 100) {
             damageBonus = 100;
-        }else {
+        } else {
             double newDamageBonus = Math.round(increaseValue * 100);
             damageBonus += (newDamageBonus / 100);
         }
     }
 
-    public void decrementDamageBonus(double decreaseValue){
-        if(decreaseValue <= 0){
+    public void decrementDamageBonus(double decreaseValue) {
+        if (decreaseValue <= 0) {
             throw new IllegalArgumentException("Decrease value must be greater than 0");
         }
-        if(decreaseValue >= 100){
+        if (decreaseValue >= 100) {
             throw new IllegalArgumentException("Decrease value must be less or equal to 100");
         }
-        if(damageBonus - decreaseValue < 0){
+        if (damageBonus - decreaseValue < 0) {
             damageBonus = 0;
-        }else {
+        } else {
             double newDamageBonus = Math.round((damageBonus - decreaseValue) * 100);
             damageBonus = newDamageBonus / 100;
         }
     }
 
-    public boolean addArmorToInventory(Armor armor){
+    public boolean addArmorToInventory(Armor armor) {
 
-        if(armor.equals(null)){
+        if (armor.equals(null)) {
             throw new NullPointerException("Armor can't be null");
         }
 

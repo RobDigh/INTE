d569b6ee0b1cab4c58078a6b5982fae6689aa911
Sigoria <appelgrenjohanna@gmail.com>
2017-10-14 21:46:09 +0200

    Modified Breed tests to account for all intended values of enum. Tests fail (expected)

--- src/test/java/entity/creature/BreedTests.java
+++ src/test/java/entity/creature/BreedTests.java
@@ -8,6 +8,15 @@ public class BreedTests {
 
     @Test
     public void testAllValuesAreThere() {
-        assertNotNull(Breed.valueOf("LABRADOR"));
+        assertNotNull(Breed.valueOf("Bulldog"));
+        assertNotNull(Breed.valueOf("Chihuahua"));
+        assertNotNull(Breed.valueOf("Dachshund"));
+        assertNotNull(Breed.valueOf("Dobermann"));
+        assertNotNull(Breed.valueOf("Greyhound"));
+        assertNotNull(Breed.valueOf("Labrador"));
+        assertNotNull(Breed.valueOf("Ovtjarka"));
+        assertNotNull(Breed.valueOf("Siberian husky"));
+        assertNotNull(Breed.valueOf("St. Bernard"));
+        assertNotNull(Breed.valueOf("Vizla"));
     }
 }

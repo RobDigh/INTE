d45c4791c1416c5525a9a69671970c66e4e1737b
Chumu-J <chumu_jamal@gmail.com>
2017-10-15 21:40:53 +0200

    Added new test: AcceptCratureByMonsterAndWinBattle
    -Imported AssertEquals to testfile
    -initialized testobjects: player, monster
    -initialized Assertions in the new testmethod
    -test will fail

--- src/test/java/entity/gameMap/AcceptTest.java
+++ src/test/java/entity/gameMap/AcceptTest.java
@@ -2,8 +2,11 @@ package entity.gameMap;
 
 import org.junit.Test;
 
+import entity.Creature;
+
 import java.awt.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.mock;
@@ -12,6 +15,8 @@ public class AcceptTest extends GameMapTest {
 
     private GameMap lowerLevel = createDefaultSizedGameMap();
     private GameMap mockUpperLevel = mock(GameMap.class);
+    private Creature player = new Creature(500, 1);
+    private Creature monster = new Creature(50, 1);
 
     @Test
     public void acceptValidEntity() {
@@ -24,5 +29,11 @@ public class AcceptTest extends GameMapTest {
     @Test(expected = IllegalArgumentException.class)
     public void acceptNullEntity() {
         lowerLevel.accept(null, mockUpperLevel);
+    }
+    
+    @Test
+    public void acceptCreatureByMonsterAndWinBattle(){
+    	assertEquals(new Point(0,0), DEFAULT_SIZED_MAP.getPosition(player));
+    	assertEquals(new Point(0,1), DEFAULT_SIZED_MAP.getPosition(monster));
     }
 }

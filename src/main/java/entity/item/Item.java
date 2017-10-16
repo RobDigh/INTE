ae141463ce60423d74c7d5c40dcf3eaac68c1e6e
Chumu-J <chumu_jamal@gmail.com>
2017-10-16 12:25:15 +0200

    Added code for test to pass
    -added stub method addItemToInventory() in Creature.java
    -removed overrided method in armor.java class
    -all objects of type Item will now call their parentclass for object
     insertion into creature inventory.
    -Test passes

--- src/main/java/entity/item/Item.java
+++ src/main/java/entity/item/Item.java
@@ -2,6 +2,7 @@ package entity.item;
 
 import entity.Creature;
 import entity.Entity;
+import entity.gameMap.GameMap;
 
 public abstract class Item extends Entity {
 
@@ -16,5 +17,21 @@ public abstract class Item extends Entity {
     }
 
     public abstract void affect(Creature creature);
-
+    
+    @Override
+    public boolean accept(Entity entity, GameMap environment){
+    	
+    	Creature visitingCreature = (Creature) entity;
+    	Item item = this;
+    	GameMap level = environment;
+    	
+    	if(item instanceof Item){
+    		visitingCreature.addItemToInventory(item);
+    		level.remove(item);
+    		return true;
+    	}
+    	
+    	return false;
+    }
+    
 }

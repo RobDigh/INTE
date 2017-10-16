818ba442fbbd41bfd2eaa4461dfc3541f877315a
Chumu-J <chumu_jamal@gmail.com>
2017-10-16 12:20:17 +0200

    Refactored acceptTest to reflect change in assertions -Test will fail

--- src/test/java/entity/gameMap/AcceptTest.java
+++ src/test/java/entity/gameMap/AcceptTest.java
@@ -58,8 +58,9 @@ public class AcceptTest extends GameMapTest {
     public void acceptCreatureByItemAndLeaveBoard(){
     	DEFAULT_SIZED_MAP.place(armor, new Point(0,1));
     	DEFAULT_SIZED_MAP.place(player, new Point(0,0));
+    	DEFAULT_SIZED_MAP.move(player, GameMap.NORTH);
     	
-    	assertEquals(new Point(0,1), DEFAULT_SIZED_MAP.getPosition(armor));
-    	assertEquals(new Point(0,0), DEFAULT_SIZED_MAP.getPosition(player));
+    	assertEquals(null, DEFAULT_SIZED_MAP.getPosition(armor));
+    	assertEquals(new Point(0,1), DEFAULT_SIZED_MAP.getPosition(player));
     }
 }

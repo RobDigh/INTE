5eba13cd8f57bd3ac8e4be6f06838c7504c4f3da
rodi0231 <rodi0231@dsv.su.se>
2017-10-16 16:56:53 +0200

    Non working addItem in Inventory.

--- src/main/java/entity/creature/Inventory.java
+++ src/main/java/entity/creature/Inventory.java
@@ -2,9 +2,22 @@ package entity.creature;
 
 import entity.item.Item;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inventory {
 
-    public void addItem(Item item, String key) {
+    private Map<String, Item> inventoryMap;
 
+    public Inventory(){
+        inventoryMap = new HashMap<>();
+    }
+
+    public void addItem(Item item, String key) {
+        inventoryMap.put(key, item);
+    }
+
+    public Map<String, Item> getInventory() {
+        return inventoryMap;
     }
 }
---
+++ src/test/java/entity/creature/InventoryTest.java
@@ -0,0 +1,15 @@
+package entity.creature;
+
+import static org.junit.Assert.*;
+
+public class InventoryTest {
+
+    //    @Test
+//    public void testInventoryMapOneItem(){
+//
+//        testCreature.addItemToInventory(mockArmor, "armor");
+////        testItemMap.put("armor", mockArmor);
+//        assertTrue(inventory.getInventory().containsKey("armor"));
+//    }
+
+}

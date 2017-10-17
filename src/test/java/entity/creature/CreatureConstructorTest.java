47592159a892d0b2e98ac40ad96aa324b9a27219
Sigoria <appelgrenjohanna@gmail.com>
2017-10-17 12:38:36 +0200

    Modified tests to prepare for merging PC and Creature classes

--- src/test/java/entity/creature/CreatureConstructorTest.java
+++ src/test/java/entity/creature/CreatureConstructorTest.java
@@ -8,43 +8,44 @@ public class CreatureConstructorTest {
 
     @Test
     public void testCreateCreature() {
-        Creature creature = new Creature(5, 6, 7);
+        Creature creature = new Creature(5, 6, 7, true);
         assertEquals(5, creature.getStrength());
         assertEquals(6, creature.getDexterity());
         assertEquals(7, creature.getConstitution());
 
         assertEquals(12, creature.getSpeed());
         assertEquals(70, creature.getHP());
+        assertEquals(Type.GREYHOUND, creature.getType());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreatureWithNegativeStr(){
-        new Creature(-1, 6, 7);
+        new Creature(-1, 6, 7, true);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreatureWithNegativeDex(){
-        new Creature(5, -1, 7);
+        new Creature(5, -1, 7, true);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreatureWithNegativeCon(){
-        new Creature(5, 6, -1);
+        new Creature(5, 6, -1, true);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreatureWithZeroStrength(){
-        new Creature(0, 6, 7);
+        new Creature(0, 6, 7, true);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreatureWithZeroDex(){
-        new Creature(5, 0, 7);
+        new Creature(5, 0, 7, true);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreatureWithZeroConstitution(){
-        new Creature(5, 6, 0);
+        new Creature(5, 6, 0, true);
     }
 
 }

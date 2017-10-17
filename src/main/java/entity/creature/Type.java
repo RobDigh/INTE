87f706a43cdb6e1a68ea3c3ba275485d0f9c85d2
Sigoria <appelgrenjohanna@gmail.com>
2017-10-17 12:34:15 +0200

    Modified Type constructor to accept new boolean argument, and enum values. Tests pass

--- src/main/java/entity/creature/Type.java
+++ src/main/java/entity/creature/Type.java
@@ -1,16 +1,16 @@
 package entity.creature;
 
 public enum Type {
-    BULLDOG("Bulldog", 6, 5, 7),
-    CHIHUAHUA("Chihuahua", 5, 8, 5),
-    DACHSHUND("Dachshund", 5, 7, 6),
-    DOBERMANN("Dobermann", 7, 6, 5),
-    GREYHOUND("Greyhound", 5, 6, 7),
-    LABRADOR("Labrador", 6, 6, 6),
-    OVTJARKA("Ovtjarka", 8, 5, 5),
-    SIBERIANHUSKY("Siberian husky", 6, 7, 5),
-    STBERNARD("St. Bernard", 7, 5, 6),
-    VIZSLA("Vizsla", 5, 5, 8);
+    BULLDOG("Bulldog", 6, 5, 7, true),
+    CHIHUAHUA("Chihuahua", 5, 8, 5, true),
+    DACHSHUND("Dachshund", 5, 7, 6, true),
+    DOBERMANN("Dobermann", 7, 6, 5, true),
+    GREYHOUND("Greyhound", 5, 6, 7, true),
+    LABRADOR("Labrador", 6, 6, 6, true),
+    OVTJARKA("Ovtjarka", 8, 5, 5, true),
+    SIBERIANHUSKY("Siberian husky", 6, 7, 5, true),
+    STBERNARD("St. Bernard", 7, 5, 6, true),
+    VIZSLA("Vizsla", 5, 5, 8, true);
 
     private String name;
     private int strength;
@@ -18,11 +18,12 @@ public enum Type {
     private int constitution;
     private boolean isPC;
 
-    Type(String name, int strength, int dexterity, int constitution) {
+    Type(String name, int strength, int dexterity, int constitution, boolean isPC) {
         this.name = name;
         this.strength = strength;
         this.dexterity = dexterity;
         this.constitution = constitution;
+        this.isPC = isPC;
     }
 
     public String getName() {

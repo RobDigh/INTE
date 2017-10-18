48ac81a9f5259ff960daac3a5a327d3b9dc4c87c
rodi0231 <rodi0231@dsv.su.se>
2017-10-18 17:11:13 +0200

    Working inventory and InventoryTests.

--- src/test/java/entity/creature/CreatureTest.java
+++ src/test/java/entity/creature/CreatureTest.java
@@ -12,7 +12,6 @@ import org.junit.rules.ExpectedException;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;

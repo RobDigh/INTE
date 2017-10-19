c72615fef1d3e6bc616de366cac3862f4e364d32
Sigoria <appelgrenjohanna@gmail.com>
2017-10-19 13:28:38 +0200

    Removed class CreatureConstructorTests and added tests from class into CreatureTests

--- src/test/java/entity/creature/CreatureTest.java
+++ src/test/java/entity/creature/CreatureTest.java
@@ -734,6 +734,53 @@ public class CreatureTest {
         verify(mockInventory).removeItem(mockSpeedPotion, "speed potion");
     }
 
+    /**
+     * Add tests for constructor
+     */
+//Start tests from CreatureConstructorTest
+    @Test
+    public void testCreateCreature() {
+        Creature creature = new Creature(5, 6, 7, true);
+        assertEquals(5, creature.getStrength());
+        assertEquals(6, creature.getDexterity());
+        assertEquals(7, creature.getConstitution());
+
+        assertEquals(12, creature.getSpeed());
+        assertEquals(70, creature.getHP());
+        assertEquals(Type.GREYHOUND, creature.getType());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreatureWithNegativeStr(){
+        new Creature(-1, 6, 7, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreatureWithNegativeDex(){
+        new Creature(5, -1, 7, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreatureWithNegativeCon(){
+        new Creature(5, 6, -1, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreatureWithZeroStrength(){
+        new Creature(0, 6, 7, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreatureWithZeroDex(){
+        new Creature(5, 0, 7, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreatureWithZeroConstitution(){
+        new Creature(5, 6, 0, true);
+    }
+// End tests from CreatureConstuctorTests
+
     @Test(expected = IllegalArgumentException.class)
     public void testConstructorInvalidValueStrength() {
         createPlayerWithCustomStrengthDexterityAndConstitution(4, 8, 5);

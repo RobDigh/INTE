a62514a30c73e605e0605cac7cb5f765a44daf7d
Simon Olofsson <s.olofsson@gmail.com>
2017-10-24 07:54:49 +0200

    Corrected GameMap.EAST and GameMap.WEST.

--- src/main/java/entity/gameMap/GameMap.java
+++ src/main/java/entity/gameMap/GameMap.java
@@ -15,8 +15,8 @@ public class GameMap extends Entity {
 
     public static final Point NORTH = new Point(0, 1);
     public static final Point SOUTH = new Point(0, -1);
-    public static final Point WEST = new Point(1, 0);
-    public static final Point EAST = new Point(-1, 0);
+    public static final Point WEST = new Point(-1, 0);
+    public static final Point EAST = new Point(1, 0);
 
     public static final Point DEFAULT_ENTRY_POSITION = new Point(0, 0);
 

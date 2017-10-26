8776334b6f9a7971098ed618cb0dfffdbfd31592
Simon Olofsson <s.olofsson@gmail.com>
2017-10-26 11:39:57 +0200

    Wrote minimal code to pass test.

--- src/main/java/combat/CombatFactory.java
+++ src/main/java/combat/CombatFactory.java
@@ -5,6 +5,6 @@ import entity.creature.Creature;
 public class CombatFactory {
 
     public Combat create(Creature c1, Creature c2) {
-        return null;
+        return new Combat(c1, c2);
     }
 }
